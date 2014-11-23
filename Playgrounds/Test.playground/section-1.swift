3d4ae71df744ade3f540ec5815bde95596675c86
YJ Park <yjpark@gmail.com>
2014-11-23 14:42:16 +0800

    create a xcode project for playground usage

---
+++ Playgrounds/Test.playground/section-1.swift
@@ -0,0 +1,86 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+import DapCore
+import DapLua
+
+var lua = DapLuaState.sharedState()
+
+var a: Item = Registry.Global.add("test")!
+
+a.addBool("bool", true)
+a.addInt("int", 1)
+a.addFloat("float", 1.1)
+a.addDouble("double", 1.1)
+a.addString("string", "str")
+
+lua.eval("result = dap.set_bool('test', 'bool', false); dap.set_bool('test', 'result', result)")
+a.getBool("bool")
+
+lua.eval("dap.set_int('test', 'int', 10)")
+a.getInt("int")
+
+lua.eval("dap.set_float('test', 'float', 10.2)")
+a.getFloat("float")
+
+lua.eval("dap.set_double('test', 'double', 1.02)")
+a.getDouble("double")
+
+lua.eval("dap.set_string('test', 'string', 'next')")
+a.getString("string")
+
+lua.eval("dap.set_string('test', 'string', tostring(package.path))")
+a.getString("string")
+
+lua.eval("dap.set_string('test', 'string', tostring(lfs.currentdir()))")
+a.getString("string")
+a.getString("string")?.lastPathComponent
+
+/*
+lua.eval("lfs.chdir('/Users/yjpark/projects/angeldnd/dap/core/swift')")
+
+lua.eval("dap.set_string('test', 'string', tostring(lfs.currentdir()))")
+a.getString("string")
+*/
+
+/*
+lua.eval("require 'init'; dap.set_string('test', 'string', tostring(lfs))")
+a.getString("string")
+
+lua.eval("e = require 'moonscript'; dap.set_string('test', 'string', tostring(e))")
+a.getString("string")
+
+lua.eval("v = require 'moonscript.version'.version; dap.set_string('test', 'string', v)")
+a.getString("string")
+
+lua.eval("t = require 'test'; dap.set_string('test', 'string', tostring(t))")
+a.getString("string")
+
+lua.eval("t = require 'test1'; dap.set_string('test', 'string', tostring(t))")
+a.getString("string")
+*/
+
+//lua.eval("require 'init'")
+
+lua.eval("dap.add_string('test', 's', 'new')")
+a.getString("s")
+
+lua.eval("print(dap.remove_string('test', 's'))")
+a.getString("s")
+
+var d = NSMutableDictionary()
+a.properties.dump(d)
+d
+var e = d.valueForKey("aspects") as NSMutableDictionary
+e.valueForKey("bool")
+e.valueForKey("string")
+
+var b: Item = Registry.Global.add("test1")!
+b.properties.load(d)
+b.getBool("bool")
+b.getString("string")
+b.getString("s")
+
+
+
