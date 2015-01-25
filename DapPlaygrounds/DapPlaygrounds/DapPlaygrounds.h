3df20f61971a6be1568b0c5f2bda0ff20d4011e2
YJ Park <yjpark@gmail.com>
2015-01-26 00:34:20 +0800

    create a new playground project

---
+++ DapPlaygrounds/DapPlaygrounds/DapPlaygrounds.h
@@ -0,0 +1,19 @@
+//
+//  DapPlaygrounds.h
+//  DapPlaygrounds
+//
+//  Created by YJ Park on 2015-1-26.
+//  Copyright (c) 2015 angeldnd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DapPlaygrounds.
+FOUNDATION_EXPORT double DapPlaygroundsVersionNumber;
+
+//! Project version string for DapPlaygrounds.
+FOUNDATION_EXPORT const unsigned char DapPlaygroundsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DapPlaygrounds/PublicHeader.h>
+
+
---
+++ DapPlaygrounds/Test.playground/section-1.swift
@@ -0,0 +1,8 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+import DapCore
+import DapMRuby
+
+var str = "Hello, playground"
