91741184c78e74cd0d8a0855d6ef070c61229610
YJ Park <yjpark@gmail.com>
2015-01-26 02:35:40 +0800

    wip

--- DapPlaygrounds/Test.playground/section-1.swift
+++ DapPlaygrounds/Test.playground/section-1.swift
@@ -5,4 +5,13 @@ import UIKit
 import DapCore
 import DapMRuby
 
+var item:Item = Registry.Global.add("test")!
+
+item.addBool("b", true)
+item.getBool("b")
+
+var state:DapMRubyState = DapMRubyState.sharedState()
+
+state.eval("puts 'test'")
+
 var str = "Hello, playground"
